9e3deafa724ffea6c566fe05a61d8125d9f7268d
Sebastian Lesmes Alvarado <slesmes1992@gmail.com>
2016-02-28 18:27:04 -0500

    - Simulador

---
+++ simuladorMapReduce/senecajs/client-test.js
@@ -0,0 +1,52 @@
+var seneca = require('seneca')()
+
+// Conexion a MongoDB:
+seneca.use('mongo-store',{
+	name: 'dbname',
+	host: '172.28.128.5',
+	port: 27017
+})
+
+var client = seneca.client({ host:'localhost', port:8080 })
+
+
+// Envia al topico de la generacion aleatoria y espera respuesta
+var iteraciones = 1; var index; var partes;
+for (var i = 0; i < iteraciones; i++) 
+{
+	index = (i+1);
+	client.act({ type: 'msGenerator', function: 'generar', vars:{"index":index}}, function (err, result)
+	{
+	  if (err)
+	  {
+	      console.error(err);
+	  } 
+	  else
+	  {
+	    var array = result.respond;
+	    // Cada posicion del arreglo es un campo para el objeto JSON a meter en Mongo:
+	    // 1. Inicializa la collecion:
+	    var record = seneca.make$('emergencias');
+	    // 2. Asigna los diferentes atributos al objeto:
+	    partes = array[0].split("|");
+	    record.timeInicial = partes[0];
+	    record.timeFinal = partes[1];
+	    record.cP1La = partes[2];
+	    record.cP1Lo = partes[3];
+	    record.cP2La = partes[4];
+	    record.cP2Lo = partes[5];
+	    record.eventosC = partes[6]; // Eventos cardiacos
+	    record.eventosR = partes[7]; // Eventos respiratorios
+	    record.eventosT = partes[8];  // Eventos temperatura
+	    // 3. Guarda el registro:
+	    record.save$(function (err,record) 
+	    {
+		    console.log( "Registro creado exitosamente en MongoDB, con ID = "+record.id)
+		})
+
+	  }
+	})
+};
+
+
+
