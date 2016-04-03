a1603ab7e54cd1eb4299b02de5e91d07b5e39c3d
qjuanp <qjuanp@gmail.com>
2016-04-02 23:07:43 -0500

    No Cassandra

--- proyecto2/spark/python/spark.py
+++ proyecto2/spark/python/spark.py
@@ -1,8 +1,3 @@
-import pyspark_cassandra
-import pyspark_cassandra.streaming
-
-from pyspark_cassandra import CassandraSparkContext
-
 from pyspark import SparkContext, SparkConf
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
@@ -10,8 +5,7 @@ from uuid import uuid1
 
 import json
 
-sc = CassandraSparkContext(conf=conf)
-sql = SQLContext(sc)
+sc = SparkContext(appName="KafkaStreaming")
 stream = StreamingContext(sc, 1) # 1 second window
 
 kafka_stream = KafkaUtils.createStream(stream, \
